2c109eff84a113d391743db1c2742cd64d61a449
Medkaffouh <kaffouhmohamed@gmail.com>
2022-04-17 22:06:45 +0000

    Single product & resource routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function(){
-    return Product::all();
-});
+Route::resource('products', ProductController::class);
 
-Route::post('/products', function(){
-    return Product::create([
-        'name' => 'Product-One',
-        'slug' => 'product-one',
-        'description' => 'This is product one',
-        'price' => '99.99'
-    ]);
-});
+// Route::get('/products', [ProductController::class, 'index']);
+
+// Route::post('/products', [ProductController::class, 'store']);
+
+// Route::get('/products/{id}', [ProductController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
