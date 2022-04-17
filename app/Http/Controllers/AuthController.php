b66d5840d218317da2d71c0f7448afd26b6fb5d9
Medkaffouh <kaffouhmohamed@gmail.com>
2022-04-17 23:44:54 +0000

    Logout and delete token then Login user and get token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    //to know how to use sanctum see laravel documentation.
     public function register(Request $request){
         $fields = $request->validate([
             'name' => 'required|string',
